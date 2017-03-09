64ab79e6c9d749c76613fc1d9111541e91e216c5
Michael Mason <michael@michaeljmason.com>
2017-03-09 14:53:09 -0800

    working on the editable fields making sure the function works properly with edited values

--- scripts.js
+++ scripts.js
@@ -5,88 +5,39 @@ function calculate() {
   dimes = 0;
   nickels = 0;
   pennys = 0;
-  while (cents >= 25) {
+  while (cents >= parseInt(document.getElementById('one').textContent)) {
     quarters += 1;
-    cents -= 25;
+    cents -= parseInt(document.getElementById('one').textContent);
       if(quarters != 0){
         document.getElementById('quarter').classList.remove('disabled');
       }
   }
-  while (cents >= 10){
+  while (cents >= parseInt(document.getElementById('two').textContent)){
     dimes += 1;
-    cents -= 10;
+    cents -= parseInt(document.getElementById('two').textContent);
       if(dimes != 0){
         document.getElementById('dime').classList.remove('disabled');
       }
   }
-  while (cents >= 5){
+  while (cents >= parseInt(document.getElementById('three').textContent)){
     nickels += 1;
-    cents -= 5;
+    cents -= parseInt(document.getElementById('three').textContent);
       if(nickels != 0){
         document.getElementById('nickel').classList.remove('disabled');
       }
   }
-  while (cents > 0) {
+  while (cents >= parseInt(document.getElementById('four').textContent)) {
     pennys += 1;
-    cents -= 1;
+    cents -= parseInt(document.getElementById('one').textContent);
       if(pennys != 0){
         document.getElementById('penny').classList.remove('disabled');
       }
   }
-
+console.group(quarters,dimes,nickels,pennys,cents);
+console.log(parseInt(document.getElementById('one').textContent));
   document.getElementById('quarter').innerHTML = '<span>' + quarters + '</span>';
   document.getElementById('dime').innerHTML = '<span>' + dimes + '</span>';
   document.getElementById('nickel').innerHTML = '<span>' + nickels + '</span>';
   document.getElementById('penny').innerHTML = '<span>' + pennys + '</span>';
   return;
 }
-/*==============================================================================
-working to make this function work properly when taking the value of the editable fields. Constant coin is 25 with 3 random numbers, work in progress. I need to zero these values out before running the function. As of now it works properly accept for the inital value is added. i.e - cents = 25, returns q = 26, initial value of q = 25 via quarters = document.getElementById('one').innerHTML; = q = parseInt(quarters); = 25 and then adds 1 when meeting the condition while loop ending with q = 26. Going to work on this tomorrow.
-
-function calculate() {
-  var cents, quarters, dimes, nickels, pennys,q,d,n,p;
-  cents = document.getElementById('cents').value;
-  quarters = document.getElementById('one').innerHTML;//get the span innerHTML value
-  dimes = document.getElementById('two').innerHTML;
-  nickels = document.getElementById('three').innerHTML;
-  pennys = document.getElementById('four').innerHTML;
-  q = parseInt(quarters);//parse string into integer
-  d = parseInt(dimes);
-  n = parseInt(nickels);
-  p = parseInt(pennys);
-  while (cents >= q.value) {
-    q += 1;
-    cents -= q.value;
-      if(q != 0){
-        document.getElementById('quarter').classList.remove('disabled');
-      }
-  }
-  while (cents >= 10){
-    d += 1;
-    cents -= 10;
-      if(d != 0){
-        document.getElementById('dime').classList.remove('disabled');
-      }
-  }
-  while (cents >= 5){
-    n += 1;
-    cents -= 5;
-      if(n != 0){
-        document.getElementById('nickel').classList.remove('disabled');
-      }
-  }
-  while (cents > 0) {
-    p += 1;
-    cents -= 1;
-      if(p != 0){
-        document.getElementById('penny').classList.remove('disabled');
-      }
-  }
-  console.group(q,d,n,p,quarters);
-  document.getElementById('quarter').innerHTML = '<span>' + q + '</span>';
-  document.getElementById('dime').innerHTML = '<span>' + d + '</span>';
-  document.getElementById('nickel').innerHTML = '<span>' + n + '</span>';
-  document.getElementById('penny').innerHTML = '<span>' + p + '</span>';
-  return;
-}
-*=========================================================================/
