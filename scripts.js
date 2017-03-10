85337cccff31980e65f012eda4a6d3e456ff92c7
Michael Mason <michael@michaeljmason.com>
2017-03-09 16:04:30 -0800

    finished the js, I have it working correctly based on the test cases given in the assignment. My experience with vanilla javaScript is rather limited. I am sure there is a better way to do this... there always is. Overall fun project, just finished so styles issues for mobile, corrected issues so that it scales appropriately. Hopefully this is good, I would love to work for NCSOFT!!!

--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
 function calculate() {
+  //declare variables
   var cents, quarters, dimes, nickels, pennys;
+
+  //gather the input value for cents
   cents = document.getElementById('cents').value;
+
+  //zero out variables - each time cents is divisible by a given value, 1 will be added to the following variables
   quarters = 0;
   dimes = 0;
   nickels = 0;
   pennys = 0;
+
+  //begin the four while loops - while cents is greater than or equal to the given value - parseInt(document.getElementById('id').textContent)) - this value will be subtracted from cents and 1 will be added to the specified variable.
   while (cents >= parseInt(document.getElementById('one').textContent)) {
     quarters += 1;
     cents -= parseInt(document.getElementById('one').textContent);
       if(quarters != 0){
-        document.getElementById('quarter').classList.remove('disabled');
+        document.getElementById('quarter').classList.remove('disabled');//if the variable is not equal to zero, remove class "disabled".
       }
   }
   while (cents >= parseInt(document.getElementById('two').textContent)){
@@ -26,16 +33,15 @@ function calculate() {
         document.getElementById('nickel').classList.remove('disabled');
       }
   }
-  while (cents >= parseInt(document.getElementById('four').textContent)) {
+  while (cents > 0) {
     pennys += 1;
-    cents -= parseInt(document.getElementById('one').textContent);
+    cents -= 1;
       if(pennys != 0){
         document.getElementById('penny').classList.remove('disabled');
       }
   }
-console.group(quarters,dimes,nickels,pennys,cents);
-console.log(parseInt(document.getElementById('one').textContent));
-  document.getElementById('quarter').innerHTML = '<span>' + quarters + '</span>';
+  console.group(quarters,dimes,nickels,pennys);//console.group variables
+  document.getElementById('quarter').innerHTML = '<span>' + quarters + '</span>';//append the output of the variables into <span>$variable</span>
   document.getElementById('dime').innerHTML = '<span>' + dimes + '</span>';
   document.getElementById('nickel').innerHTML = '<span>' + nickels + '</span>';
   document.getElementById('penny').innerHTML = '<span>' + pennys + '</span>';
